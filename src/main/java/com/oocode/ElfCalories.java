56aa676fe5967d5c88f900775d5366573e7e2162
KaderTarlann <kadertarlan1@gmail.com>
2024-04-23 11:12:27 +0100

    developed implamentation of third test

--- src/main/java/com/oocode/ElfCalories.java
+++ src/main/java/com/oocode/ElfCalories.java
@@ -1,10 +1,16 @@
 package com.oocode;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ElfCalories {
 
 
     public int getMostCalories(String input) {
-        return Integer.parseInt(input);
+        List<String> nums = Arrays.asList(input.split("\n"));
+        return nums.stream()
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 }
 
