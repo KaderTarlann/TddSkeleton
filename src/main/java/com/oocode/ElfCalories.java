aae366bffbfd9ea45a81d9a2143e1d160f811d28
KaderTarlann <kadertarlan1@gmail.com>
2024-04-23 10:49:44 +0100

    add failing test

---
+++ src/main/java/com/oocode/ElfCalories.java
@@ -0,0 +1,11 @@
+package com.oocode;
+
+public class ElfCalories {
+
+
+    public int getMostCalories(String input) {
+        throw new RuntimeException("Cannot find calories");
+    }
+}
+
+
--- src/test/java/com/oocode/ElfTest.java
+++ src/test/java/com/oocode/ElfTest.java
@@ -1,13 +1,23 @@
 package com.oocode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class ElfTest {
 
-    @Test
-    public void getMostCaloriesTest() {
+    private ElfCalories elfCalories;
 
+    @BeforeEach
+    public void setUp() {
+        elfCalories = new ElfCalories();
+    }
+
+    @Test()
+    public void getMostCaloriesTest() {
+        assertThrows(RuntimeException.class, () -> elfCalories.getMostCalories(""));
     }
 
 
